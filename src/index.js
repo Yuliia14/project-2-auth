a31955d1000ad30cd72bbe499f0cde4898c7c857
Yuliia Bevko <bevko.yulka@gmail.com>
2020-12-10 00:29:38 +0200

    dfsfd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './sass/auth-modal.scss'
+
+    const openModalBtn = document.querySelector('.auth-modal-open-button');
+    const closeModalBtn = document.querySelector('.auth-modal-close-button');
+    const authBackdropEl = document.querySelector('.auth-backdrop');
+    const authModalEl = document.querySelector('.auth-modal');
+
+  openModalBtn.addEventListener("click", openAuthModal());
+
+  function openAuthModal() {
+      console.log('dfdfdsfsdf'),
+          authBackdropEl.classList.remove("is-hidden"),
+          closeModalBtn.addEventListener("click", closeAuthModal())
+      }
+  
+      function closeAuthModal() {
+        authBackdropEl.classList.add("is-hidden");
+    }
